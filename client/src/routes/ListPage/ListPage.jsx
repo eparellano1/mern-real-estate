a881237772774a823876b92644984d5d0fc7a079
eparellano1 <eparellano1@up.edu.ph>
2024-04-03 16:16:53 +0800

    add new components

--- client/src/routes/ListPage/ListPage.jsx
+++ client/src/routes/ListPage/ListPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { listData } from '../../lib/dummydata'
+import "./ListPage.scss"
+import Filter from '../../components/Filter/Filter'
+import Card from '../../components/Card/Card'
+import Map from '../../components/Map/Map'
 
 const ListPage = () => {
+  const data = listData
+
   return (
-    <div>ListPage</div>
+    <div className='listPage'>
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item}/>
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">
+            <Map items={data}/>
+      </div>
+    </div>
   )
 }
 
