a36602e18358da9b25e080729c48c8d7fa7b43c1
eparellano1 <eparellano1@up.edu.ph>
2024-04-08 13:06:27 +0800

    add crud for posts

--- api/routes/postRouter.js
+++ api/routes/postRouter.js
@@ -1,9 +1,13 @@
 import express from "express"
+import { verifyToken } from "../middleware/verifyToken.js"
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/postCtrl.js"
+
 const postRouter = express.Router()
 
-postRouter.get("/test", (req,res) => {
-    console.log("Post Router")
-    res.send("Post Router")
-})
+postRouter.get("/", getPosts)
+postRouter.get("/:id", getPost)
+postRouter.post("/", verifyToken, addPost)
+postRouter.put("/:id", verifyToken, updatePost)
+postRouter.delete("/:id", verifyToken, deletePost)
 
 export default postRouter
---
+++ client/src/routes/NewPostPage/NewPostPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./NewPostPage.scss"
+
+const NewPostPage = () => {
+  return (
+    <div className='newPostPage'>NewPostPage</div>
+  )
+}
+
+export default NewPostPage
