44da9ec279df25d991c597689c41c804055a6a18
eparellano1 <eparellano1@up.edu.ph>
2024-04-08 11:20:15 +0800

    add update avatar using cloudinary

--- client/src/routes/ProfileUpdatePage/ProfileUpdatePage.jsx
+++ client/src/routes/ProfileUpdatePage/ProfileUpdatePage.jsx
@@ -2,32 +2,37 @@ import { useContext, useState } from "react";
 import "./ProfileUpdatePage.scss";
 import { AuthContext } from "../../context/AuthContext";
 import apiRequest from "../../lib/apiRequest";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import UploadWidget from "../../components/UploadWidget/UploadWidget";
 
 const ProfileUpdatePage = () => {
-  const [error, setError] = useState("")
-  const { currentUser, updateUser } = useContext(AuthContext)
+  const { currentUser, updateUser } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [avatar, setAvatar] = useState(currentUser.avatar);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    const formData = new FormData(e.target)
+    e.preventDefault();
+    const formData = new FormData(e.target);
 
-    const { username, email, password } = Object.fromEntries(formData)
+    const { username, email, password } = Object.fromEntries(formData);
 
     try {
       const response = await apiRequest.put(`/users/${currentUser.id}`, {
-        username, email, password
-      })
+        username,
+        email,
+        password,
+        avatar
+      });
 
-      updateUser(response.data)
-      navigate("/profile")
+      updateUser(response.data);
+      navigate("/profile");
     } catch (error) {
-      console.log(error)
-      setError(error.response.data.message)
+      console.log(error);
+      setError(error.response.data.message);
     }
-  }
+  };
 
   return (
     <div className="profileUpdatePage">
@@ -36,22 +41,42 @@ const ProfileUpdatePage = () => {
           <h1>Update Profile</h1>
           <div className="item">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" defaultValue={currentUser.username}/>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              defaultValue={currentUser.username}
+            />
           </div>
           <div className="item">
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email" defaultValue={currentUser.email}/> 
+            <input
+              type="text"
+              id="email"
+              name="email"
+              defaultValue={currentUser.email}
+            />
           </div>
           <div className="item">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" /> 
+            <input type="password" id="password" name="password" />
           </div>
           <button>Update</button>
           {error && {}}
         </form>
       </div>
       <div className="sideContainer">
-        <img src={currentUser.avatar || "/noavatar.jpg"} alt="" className="avatar" />
+        <img src={avatar || "/noavatar.jpg"} alt="" className="avatar" />
+        <UploadWidget
+          uwConfig={{
+            cloudName: "dhiwppmy9",
+            uploadPreset: "estate",
+            multiple: false,
+            maxImageFileSize: 5000000,
+            folder: "avatars",
+          }}
+          setAvatar={setAvatar}
+        />
       </div>
     </div>
   );
